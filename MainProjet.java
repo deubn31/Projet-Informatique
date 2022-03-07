eef4512e9b7cdbd6985adf7699069e2cbc2bf829
deubn31 <101141468+deubn31@users.noreply.github.com>
2022-03-07 21:11:09 +0100

    Add files via upload
    Ce qu'on a fait avec Antoine de 8h à 10h lundi 07/03

---
+++ MainProjet.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.ImageIcon;
+import java.awt.event.*;
+import java.util.LinkedList;
+import javax.swing.UIManager;
+ 
+public class MainProjet{
+  
+	public static void main(String[] args){
+		
+		// Création de la fenêtre pour l'IHM
+		FenetreProjet maFenetreProjet = new FenetreProjet();
+   }
+   
+}
+
