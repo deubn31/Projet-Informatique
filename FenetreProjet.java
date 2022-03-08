72b750d4af53163c2ee7b02bb536ee6e33ded6dd
Lysquid <romainbenoit21560@gmail.com>
2022-03-08 12:49:57 +0100

    affichage du fond simplifié

--- FenetreProjet.java
+++ FenetreProjet.java
@@ -1,43 +1,40 @@
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
 
-public class FenetreProjet extends JFrame{
-	  
-	public ImageIcon icon = new ImageIcon(".\\Images\\image-fond.png");
-    
-    	public FenetreProjet(){
-		
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+
+public class FenetreProjet extends JFrame {
+
+	Image img;
+
+	public FenetreProjet() {
+
 		// Pour ameliorer la compatibilite des affichages
 		try {
-		    UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
-		} catch(Exception e) {
-		    e.printStackTrace(); 
+			UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 
 		this.setTitle("IHM Projet - Fenetre de jeu ");
 		// Pour placer la fenêtre au centre de l'écran
 		this.setLocationRelativeTo(null);
 		// Pour empêcher le redimensionnement de la fenêtre
-		this.setResizable(false);
+		// this.setResizable(false);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setSize(1515,890);
-		this.setLocation(0,0);
-		
-		JTextArea text = new JTextArea() 
-		{
-		  Image img = icon.getImage();
-		  // initialiseur d'instance
-		  {setOpaque(false);}
-		  public void paintComponent(Graphics graphics) 
-		  {
-			graphics.drawImage(img, 0, 0, this);
-			super.paintComponent(graphics);
-		  }
-		};
-		JScrollPane pane = new JScrollPane(text);
-		Container content = this.getContentPane();
-		content.add(pane, BorderLayout.CENTER);
-		
+		this.setSize(1515, 890);
+		this.setLocation(0, 0);
+
+		Toolkit T = Toolkit.getDefaultToolkit();
+		img = T.getImage("image-fond.png");
+
 		this.setVisible(true);
-	  }
+	}
+
+	public void paint(Graphics g) {
+		g.drawImage(img, 0, 0, this);
+	}
+
 }
--- MainProjet.java
+++ MainProjet.java
@@ -1,17 +1,9 @@
-import java.awt.*;
-import javax.swing.*;
-import javax.swing.ImageIcon;
-import java.awt.event.*;
-import java.util.LinkedList;
-import javax.swing.UIManager;
- 
-public class MainProjet{
-  
-	public static void main(String[] args){
-		
+public class MainProjet {
+
+	public static void main(String[] args) {
+
 		// Création de la fenêtre pour l'IHM
 		FenetreProjet maFenetreProjet = new FenetreProjet();
-   }
-   
-}
+	}
 
+}
