4b9df35c51ab828153ac4976baf63a876f8cc27b
deubn31 <nicolas.debarge@insa-lyon.fr>
2022-03-13 00:50:37 +0100

    J'ai juste modifié les couleurs et utilisé une class sur internet pour faire des JPanels avec des coins arrondis

--- FenetreBoutons.java
+++ FenetreBoutons.java
@@ -17,36 +17,42 @@ public class FenetreBoutons extends JFrame implements ActionListener{
         JPanel conteneurHaut = new JPanel();
         conteneurHaut.setLayout(null);
         conteneurHaut.setBounds(0,0,1920,1080);
-        conteneurHaut.setBackground(Color.blue);
+        conteneurHaut.setBackground(new Color(119,181,254));
+
 
         //label titre       
         JLabel Titre = new JLabel();
         Titre.setText("Titre du jeu");
         Titre.setBounds(920,10,100,100);
-        
+
+
         //joueur1
-        JPanel conteneurJ1 = new JPanel();
+        RoundedPanel conteneurJ1 = new RoundedPanel();
         conteneurJ1.setLayout(null);
         conteneurJ1.setBounds(100,400,800,500);
-        conteneurJ1.setBackground(Color.red); 
+        conteneurJ1.setBackground(new Color(196,52,45)); 
+        conteneurJ1.setArcs(new Dimension(50, 50));
+        
         JLabel TitreJ1 = new JLabel();
         TitreJ1.setText("Joueur 1");
         TitreJ1.setBounds(470,320,100,100);   
         
         //joueur2
-        JPanel conteneurJ2 = new JPanel();
+        RoundedPanel conteneurJ2 = new RoundedPanel();
         conteneurJ2.setLayout(null);
         conteneurJ2.setBounds(1000,400,800,500);
-        conteneurJ2.setBackground(Color.yellow);
+        conteneurJ2.setBackground(new Color(255,206,0));
+        conteneurJ2.setArcs(new Dimension(50, 50));
+
         JLabel TitreJ2 = new JLabel();
         TitreJ2.setText("Joueur 2");
-        TitreJ2.setBounds(1380,320,100,100);   
-        
+        TitreJ2.setBounds(1380,320,100,100);  
+
         conteneurHaut.add(Titre);
         conteneurHaut.add(conteneurJ1);
         conteneurHaut.add(TitreJ1); 
         conteneurHaut.add(conteneurJ2);
-        conteneurHaut.add(TitreJ2);        
+        conteneurHaut.add(TitreJ2); 
         this.add(conteneurHaut);
     }
     public void actionPerformed (ActionEvent e){
