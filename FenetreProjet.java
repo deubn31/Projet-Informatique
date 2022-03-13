d59d7a6afa686f58d526e998e49342adb079ab47
deubn31 <nicolas.debarge@insa-lyon.fr>
2022-03-13 11:53:13 +0100

    Gestion taille fenetre jeu

--- FenetreProjet.java
+++ FenetreProjet.java
@@ -2,12 +2,16 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Toolkit;
 import javax.swing.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
 
 public class FenetreProjet extends JFrame {
 
-	Image img;
+	BufferedImage image;
 
-	public FenetreProjet() {
+	public FenetreProjet() throws IOException {
 
 		// Pour ameliorer la compatibilite des affichages
 		try {
@@ -20,19 +24,29 @@ public class FenetreProjet extends JFrame {
 		// Pour placer la fenêtre au centre de l'écran
 		this.setLocationRelativeTo(null);
 		// Pour empêcher le redimensionnement de la fenêtre
+		this.setResizable(false);
 		// this.setResizable(false);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setSize(1515, 890);
 		this.setLocation(0, 0);
 
+		/*
 		Toolkit T = Toolkit.getDefaultToolkit();
 		img = T.getImage("Images/image-fond.png");
+		reader.setInput
+		int x = img.getWidth(this);
+		*/
 
+        JPanel pane = new JPanel();
+		image = ImageIO.read(new File("Images\\image-fond.png"));
+		int x = image.getWidth();
+		int y = image.getHeight();
+
+		this.setSize(x, y);
 		this.setVisible(true);
 	}
 
 	public void paint(Graphics g) {
-		g.drawImage(img, 0, 0, this);
+		g.drawImage(image, 0, 0, this);
 	}
 
 }
--- MainProjet.java
+++ MainProjet.java
@@ -1,6 +1,8 @@
+import java.io.IOException;
+
 public class MainProjet {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		// Création de la fenêtre pour l'IHM
 		FenetreProjet maFenetreProjet = new FenetreProjet();
