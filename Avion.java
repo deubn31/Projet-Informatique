50042c8152094a6657a65d5ca7e6feed5ee3a623
Antom03 <antoine.meille@free.fr>
2022-03-14 08:41:37 +0100

    Classe Avion

---
+++ Avion.java
@@ -0,0 +1,11 @@
+import javax.swing.ImageIcon;
+
+public class Avion {
+    int vie = 3;
+    boolean missileCharge = true;
+    ImageIcon skin;
+
+    public Avion (ImageIcon image) {
+        skin = image;
+    }
+}
