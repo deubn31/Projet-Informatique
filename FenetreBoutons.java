2d9360321a740c9a62a54706db7088e8c9815737
Theor11 <therousselotp@gmail.com>
2022-03-14 08:53:38 +0100

    amelioration1

--- FenetreBoutons.java
+++ FenetreBoutons.java
@@ -6,7 +6,7 @@ import java.awt.event.*;
 
 public class FenetreBoutons extends JFrame implements ActionListener{
 
-    private Font policeTitre = new FontUIResource("Serif", Font.BOLD, 70);
+    private Font policeTitre = new FontUIResource("Serif", Font.BOLD, 60);
     private Font policeJoueur = new FontUIResource("Verdana", Font.BOLD, 30);
 
     private Color jaune = new Color(255,206,0);
@@ -28,45 +28,74 @@ public class FenetreBoutons extends JFrame implements ActionListener{
         conteneurHaut.setBounds(0,0,1920,1080);
         conteneurHaut.setBackground(new Color(119,181,254));
 
-
         //label titre       
+        RoundedPanel conteneurTitre = new RoundedPanel();
+        conteneurTitre.setLayout(null);
+        conteneurTitre.setBounds(700,50,500,110);
+        conteneurTitre.setBackground(Color.magenta);
+        conteneurTitre.setArcs(new Dimension(50, 50));
+
         JLabel Titre = new JLabel();
         Titre.setText("Titre du jeu");
-        Titre.setBounds((int) (this.getWidth()/2.5),10,1000,100);
+        conteneurTitre.add(Titre);
+        Titre.setBounds(90,0,400,100);
         Titre.setFont(policeTitre);
-
 
         //joueur1
         RoundedPanel conteneurJ1 = new RoundedPanel();
         conteneurJ1.setLayout(null);
-        conteneurJ1.setBounds(100,400,800,500);
+        conteneurJ1.setBounds(100,300,800,500);
         conteneurJ1.setBackground(rouge); 
         conteneurJ1.setArcs(new Dimension(50, 50));
         
         JLabel TitreJ1 = new JLabel();
         TitreJ1.setText("Joueur 1");
-        TitreJ1.setBounds((int) (100 + conteneurJ1.getWidth()/2.5),320,400,100);
+        TitreJ1.setBounds((int) (100 + conteneurJ1.getWidth()/2.5),220,400,100);
         TitreJ1.setFont(policeJoueur);
         TitreJ1.setForeground(rouge);
+
+        JLabel choixJ1 = new JLabel("Choisissez votre avion");
+        conteneurJ1.add(choixJ1);
+        choixJ1.setBounds(350,400,300,100);
         
         //joueur2
         RoundedPanel conteneurJ2 = new RoundedPanel();
         conteneurJ2.setLayout(null);
-        conteneurJ2.setBounds(1000,400,800,500);
+        conteneurJ2.setBounds(1000,300,800,500);
         conteneurJ2.setBackground(jaune);
         conteneurJ2.setArcs(new Dimension(50, 50));
 
         JLabel TitreJ2 = new JLabel();
         TitreJ2.setText("Joueur 2");
-        TitreJ2.setBounds((int) (1000 + conteneurJ2.getWidth()/2.5),320,400,100); 
+        TitreJ2.setBounds((int) (1000 + conteneurJ2.getWidth()/2.5),220,400,100); 
         TitreJ2.setFont(policeJoueur);
         TitreJ2.setForeground(jaune); 
+        
+        JLabel choixJ2 = new JLabel("Choisissez votre avion");
+        conteneurJ2.add(choixJ2);
+        choixJ2.setBounds(350,400,300,100);
 
-        conteneurHaut.add(Titre);
+        //bouton jouer
+        JButton jouer = new JButton ("JOUER");
+        jouer.setLayout(null);
+        jouer.setBounds(850,850,200,100);
+        jouer.setBackground(Color.green);
+        jouer.addActionListener(this);
+
+        //bouton commande
+        JButton comm = new JButton ("Commandes");
+        comm.setLayout(null);
+        comm.setBounds(1550,850,200,100);
+        comm.setBackground(Color.cyan);
+        comm.addActionListener(this);
+
+        conteneurHaut.add(conteneurTitre);
         conteneurHaut.add(conteneurJ1);
         conteneurHaut.add(TitreJ1); 
         conteneurHaut.add(conteneurJ2);
-        conteneurHaut.add(TitreJ2); 
+        conteneurHaut.add(TitreJ2);  
+        conteneurHaut.add(jouer);
+        conteneurHaut.add(comm);
         this.add(conteneurHaut);
     }
     public void actionPerformed (ActionEvent e){
