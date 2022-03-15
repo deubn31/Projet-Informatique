ec2db9cfdc841df9643d9970078feac4845f1413
deubn31 <nicolas.debarge@insa-lyon.fr>
2022-03-15 21:35:47 +0100

    Commandes Joueur 1 finalisées

--- FenetreProjet.java
+++ FenetreProjet.java
@@ -8,7 +8,7 @@ public class FenetreProjet extends JFrame implements KeyListener, ActionListener
 
 	public BufferedImage image;
 	public Avion AvionJ1;
-	public int pas = 10;
+	public int pas = 20;
 	public HashSet<Integer> evenementClavier = new HashSet<Integer>();
 
 	public FenetreProjet() throws IOException {
@@ -27,56 +27,57 @@ public class FenetreProjet extends JFrame implements KeyListener, ActionListener
 		// this.setResizable(false);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		//Implémentation KeyListener
+		// Implémentation KeyListener
 		this.addKeyListener(this);
 
-		//Panel Principal
+		// Panel Principal
 		JPanel Principal = new JPanel();
 		Principal.setLayout(null);
 
-		//Panel de l'Image de fond
+		// JPanel de l'Image de fond
 		JPanelBackground Conteneur = new JPanelBackground();
 
-		//Réglage de la taille de la fenêtre en fonction de l'image de fond
+		// Réglage de la taille de la fenêtre en fonction de l'image de fond
 		this.setSize(1515, 890);
 
-		//Images de points de Vie
+		// Images de points de Vie
 		ImageIcon troisPointsDeVie = new ImageIcon("Images/3viesmodif.png");
 		ImageIcon deuxPointsDeVie;
 		ImageIcon unPointDeVie;
 		ImageIcon zeroPointDeVie;
 
-		//PP avions
+		// PP avions
 		ImageIcon PPJoueur1 = new ImageIcon("Images/AvionVertmodif.png");
 		ImageIcon PPJoueur2 = new ImageIcon("Images/AvionVioletmodif.png");
 
-		//Skin avions
+		// Skin avions
 		ImageIcon skinAvionViolet = new ImageIcon("Images/SkinAvionVioletDroitemodif.png");
 		ImageIcon skinAvionRouge = new ImageIcon("Images/SkinAvionRouge.png");
 
-		//----------- JOUEUR 1 --------------//
+		// ----------- JOUEUR 1 --------------//
 
-		//Points de vie Joueur1
-        JLabel vie1J1 = new JLabel(troisPointsDeVie);
-		vie1J1.setBounds(100,10,troisPointsDeVie.getIconWidth(),troisPointsDeVie.getIconHeight());
+		// Points de vie Joueur1
+		JLabel vie1J1 = new JLabel(troisPointsDeVie);
+		vie1J1.setBounds(100, 10, troisPointsDeVie.getIconWidth(), troisPointsDeVie.getIconHeight());
 
-		//Photo de profil Joueur1
+		// Photo de profil Joueur1
 		JLabel PPJ1 = new JLabel(PPJoueur1);
-		PPJ1.setBounds(10,10,PPJoueur1.getIconWidth(), PPJoueur1.getIconHeight());
+		PPJ1.setBounds(10, 10, PPJoueur1.getIconWidth(), PPJoueur1.getIconHeight());
 
-		//Avion du J1
-		AvionJ1 = new Avion(skinAvionViolet); 
+		// Avion du J1
+		AvionJ1 = new Avion(skinAvionViolet);
 
-		//----------- JOUEUR 2 --------------//
+		// ----------- JOUEUR 2 --------------//
 
-		//Points de vie Joueur2
-        JLabel vie1J2 = new JLabel(troisPointsDeVie);
-		vie1J2.setBounds(this.getWidth()-troisPointsDeVie.getIconWidth()-110,10,troisPointsDeVie.getIconWidth(),troisPointsDeVie.getIconHeight());
+		// Points de vie Joueur2
+		JLabel vie1J2 = new JLabel(troisPointsDeVie);
+		vie1J2.setBounds(this.getWidth() - troisPointsDeVie.getIconWidth() - 110, 10, troisPointsDeVie.getIconWidth(),
+				troisPointsDeVie.getIconHeight());
 
-		//Photo de profil Joueur2
+		// Photo de profil Joueur2
 		JLabel PPJ2 = new JLabel(PPJoueur2);
-		PPJ2.setBounds(this.getWidth()-PPJoueur2.getIconWidth()-25,10,PPJoueur2.getIconWidth(), PPJoueur2.getIconHeight());
-
+		PPJ2.setBounds(this.getWidth() - PPJoueur2.getIconWidth() - 25, 10, PPJoueur2.getIconWidth(),
+				PPJoueur2.getIconHeight());
 
 		Principal.add(vie1J1);
 		Principal.add(vie1J2);
@@ -89,7 +90,7 @@ public class FenetreProjet extends JFrame implements KeyListener, ActionListener
 
 		this.setVisible(false);
 
-		Timer mt = new Timer(16,this);
+		Timer mt = new Timer(16, this);
 		mt.start();
 	}
 
@@ -105,76 +106,82 @@ public class FenetreProjet extends JFrame implements KeyListener, ActionListener
 	@Override
 	public void keyPressed(KeyEvent e) {
 		evenementClavier.add(e.getKeyCode());
-		/*
-		switch(e.getKeyCode()){
-			case 37 : 
-				if (AvionJ1.posX>=10){
-					AvionJ1.updatePos(AvionJ1.posX-pas, AvionJ1.posY);
-				}
-				break;
-			case 39 : 
-				if (AvionJ1.posX <= this.getWidth()-170){
-					AvionJ1.updatePos(AvionJ1.posX+pas, AvionJ1.posY);
-				}
-				break;
-			case 38 : 
-				if (AvionJ1.posY>=10){
-					AvionJ1.updatePos(AvionJ1.posX, AvionJ1.posY-(pas/2));
-				}
-				break;
-			case 40 : 
-				if (AvionJ1.posY <= this.getHeight()-100){
-					AvionJ1.updatePos(AvionJ1.posX, AvionJ1.posY+(pas/2));
-				}
-				break;
-			case 32 :
-				if (pas == 10){
-					pas = 50;
-				} else {
-					pas = 10;
-				}
-				break;
-		}
-		*/
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if((evenementClavier.contains(KeyEvent.VK_D)) && !(evenementClavier.contains(KeyEvent.VK_Z) || (evenementClavier.contains(KeyEvent.VK_S)))) {
-			if (AvionJ1.posX <= this.getWidth()-170){
-				AvionJ1.updatePos(AvionJ1.posX+pas, AvionJ1.posY);
+
+		// Gestion des touches zqsd du Joueur 1 //
+
+		if (evenementClavier.contains(KeyEvent.VK_D)) {
+			if (evenementClavier.contains(KeyEvent.VK_Z)) {
+				if (AvionJ1.posX <= this.getWidth() - 170){
+					if (AvionJ1.posY >= 10) {
+						AvionJ1.updatePos(AvionJ1.posX + pas, AvionJ1.posY);
+						AvionJ1.updatePos(AvionJ1.posX, AvionJ1.posY - (pas / 2));
+					} else {
+						AvionJ1.updatePos(AvionJ1.posX + pas, AvionJ1.posY);
+					}
+				} else if (AvionJ1.posY >= 10) {
+					AvionJ1.updatePos(AvionJ1.posX, AvionJ1.posY - (pas / 2));
+				}
+			} else if (evenementClavier.contains(KeyEvent.VK_S)) {
+				if (AvionJ1.posX <= this.getWidth() - 170) {
+					if (AvionJ1.posY <= this.getHeight() - 100) {
+						AvionJ1.updatePos(AvionJ1.posX + pas, AvionJ1.posY);
+						AvionJ1.updatePos(AvionJ1.posX, AvionJ1.posY + (pas / 2));
+					} else {
+						AvionJ1.updatePos(AvionJ1.posX + pas, AvionJ1.posY);
+					}
+				} else if (AvionJ1.posY <= this.getHeight() - 100) {
+					AvionJ1.updatePos(AvionJ1.posX, AvionJ1.posY + (pas / 2));
+				}
+			} else if (evenementClavier.contains(KeyEvent.VK_Q)) {
+				AvionJ1.updatePos(AvionJ1.posX, AvionJ1.posY);
+			} else {
+				if (AvionJ1.posX <= this.getWidth() - 170) {
+					AvionJ1.updatePos(AvionJ1.posX + pas, AvionJ1.posY);
+				}
 			}
-		} else if((evenementClavier.contains(KeyEvent.VK_Q)) && !(evenementClavier.contains(KeyEvent.VK_Z) || (evenementClavier.contains(KeyEvent.VK_S)))) {
-			if (AvionJ1.posX>=10){
-				AvionJ1.updatePos(AvionJ1.posX-pas, AvionJ1.posY);
+		} else if (evenementClavier.contains(KeyEvent.VK_Q)) {
+			if (evenementClavier.contains(KeyEvent.VK_Z)) {
+				if (AvionJ1.posX >= 10){
+					if (AvionJ1.posY >= 10) {
+						AvionJ1.updatePos(AvionJ1.posX - pas, AvionJ1.posY);
+						AvionJ1.updatePos(AvionJ1.posX, AvionJ1.posY - (pas / 2));
+					} else {
+						AvionJ1.updatePos(AvionJ1.posX - pas, AvionJ1.posY);
+					}
+				} else if (AvionJ1.posY >= 10){
+					AvionJ1.updatePos(AvionJ1.posX, AvionJ1.posY - (pas / 2));
+				}
+			} else if (evenementClavier.contains(KeyEvent.VK_S)) {
+				if (AvionJ1.posX >= 10) {
+					if (AvionJ1.posY <= this.getHeight() - 100) {
+						AvionJ1.updatePos(AvionJ1.posX - pas, AvionJ1.posY);
+						AvionJ1.updatePos(AvionJ1.posX, AvionJ1.posY + (pas / 2));
+					} else {
+						AvionJ1.updatePos(AvionJ1.posX - pas, AvionJ1.posY);
+					}
+				} else if (AvionJ1.posY <= this.getHeight() - 100){
+					AvionJ1.updatePos(AvionJ1.posX, AvionJ1.posY + (pas / 2));
+				}
+			} else {
+				if (AvionJ1.posX >= 10) {
+					AvionJ1.updatePos(AvionJ1.posX - pas, AvionJ1.posY);
+				}
 			}
-		} else if((evenementClavier.contains(KeyEvent.VK_S)) && !(evenementClavier.contains(KeyEvent.VK_D) || (evenementClavier.contains(KeyEvent.VK_Q)))) {
-			if (AvionJ1.posY <= this.getHeight()-100){
-				AvionJ1.updatePos(AvionJ1.posX, AvionJ1.posY+(pas/2));
+		} else if (evenementClavier.contains(KeyEvent.VK_S)) {
+			if (evenementClavier.contains(KeyEvent.VK_Z)) {
+				AvionJ1.updatePos(AvionJ1.posX, AvionJ1.posY);
+			} else {
+				if (AvionJ1.posY <= this.getHeight() - 100) {
+					AvionJ1.updatePos(AvionJ1.posX, AvionJ1.posY + (pas / 2));
+				}
 			}
-		} else if((evenementClavier.contains(KeyEvent.VK_Z)) && !(evenementClavier.contains(KeyEvent.VK_D) || (evenementClavier.contains(KeyEvent.VK_Q)))) {
-			if (AvionJ1.posY>=10){
-				AvionJ1.updatePos(AvionJ1.posX, AvionJ1.posY-(pas/2));
-			}
-		} else if((evenementClavier.contains(KeyEvent.VK_Q)) && (evenementClavier.contains(KeyEvent.VK_Z))){
-			if ((AvionJ1.posX>=10) && (AvionJ1.posY>=10)){
-				AvionJ1.updatePos(AvionJ1.posX-pas, AvionJ1.posY);
-				AvionJ1.updatePos(AvionJ1.posX, AvionJ1.posY-(pas/2));
-			}
-		} else if((evenementClavier.contains(KeyEvent.VK_D)) && (evenementClavier.contains(KeyEvent.VK_Z))){
-			if ((AvionJ1.posX <= this.getWidth()-170) && (AvionJ1.posY>=10)){
-				AvionJ1.updatePos(AvionJ1.posX+pas, AvionJ1.posY);
-				AvionJ1.updatePos(AvionJ1.posX, AvionJ1.posY-(pas/2));
-			}
-		} else if((evenementClavier.contains(KeyEvent.VK_D)) && (evenementClavier.contains(KeyEvent.VK_S))){
-			if ((AvionJ1.posX <= this.getWidth()-170) && (AvionJ1.posY <= this.getHeight()-100)){
-				AvionJ1.updatePos(AvionJ1.posX+pas, AvionJ1.posY);
-				AvionJ1.updatePos(AvionJ1.posX, AvionJ1.posY+(pas/2));
-			}
-		} else if((evenementClavier.contains(KeyEvent.VK_Q)) && (evenementClavier.contains(KeyEvent.VK_S))){
-			if ((AvionJ1.posX>=10) && (AvionJ1.posY <= this.getHeight()-100)){
-				AvionJ1.updatePos(AvionJ1.posX-pas, AvionJ1.posY);
-				AvionJ1.updatePos(AvionJ1.posX, AvionJ1.posY+(pas/2));
+		} else if (evenementClavier.contains(KeyEvent.VK_Z)) {
+			if (AvionJ1.posY >= 10) {
+				AvionJ1.updatePos(AvionJ1.posX, AvionJ1.posY - (pas / 2));
 			}
 		}
 	}
