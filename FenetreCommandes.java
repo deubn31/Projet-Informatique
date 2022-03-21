3324934ed15d6262f3b8de2927021abc98f54f70
Theor11 <therousselotp@gmail.com>
2022-03-21 11:16:55 +0100

    jsp

--- FenetreCommandes.java
+++ FenetreCommandes.java
@@ -3,16 +3,26 @@ import javax.swing.plaf.FontUIResource;
 import javafx.event.ActionEvent;
 import java.awt.*;
 import java.awt.event.*;
-import java.io.IOException;
 
 public class FenetreCommandes extends JFrame implements ActionListener{
-    
-    public FenetreCommandes (){
 
-        this.setTitle("Menu Commandes");
+    private Color gris = new Color(169,169,169);
+    private Font policeRetour = new FontUIResource("Verdana", Font.BOLD, 30);
+
+    JButton retour;
+
+    public FenetreCommandes(){
+
+        this.setTitle("Menu retour");
         this.setSize(1950, 1080);
 
-
+        //bouton retour au menu
+        retour = new JButton ("Retour");
+        retour.setLayout(null);
+        retour.setBounds(1550,850,300,100);
+        retour.setBackground(gris);
+        retour.setFont(policeRetour);
+        retour.addActionListener(this);
 
 
 
@@ -23,5 +33,8 @@ public class FenetreCommandes extends JFrame implements ActionListener{
     }
 
     public void actionPerformed (ActionEvent e) {
+        if(e.getSource()==retour){
+            this.setVisible(false);
+        }
     }
 }
