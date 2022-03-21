89fc76d3a62418a98cb6dc25e70f70d26567ec74
Theor11 <therousselotp@gmail.com>
2022-03-21 10:35:25 +0100

    debut fenetre des commandes

---
+++ FenetreCommandes.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+import javax.swing.plaf.FontUIResource;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.IOException;
+
+public class FenetreCommandes extends JFrame implements ActionListener{
+    
+    public FenetreCommandes (){
+
+        this.setTitle("Menu Commandes");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(1950, 1080);
+
+
+
+
+
+
+
+
+        this.setVisible(false);
+    }
+
+    public void actionPerformed(ActionEvent e){
+
+    }
+}
